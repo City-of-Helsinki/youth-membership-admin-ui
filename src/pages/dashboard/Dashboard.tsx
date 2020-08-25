1cdec31cc8051b2016cc739d3f7aad1cc0c8749d
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-08-25 10:24:57 +0300

    Redirect user after login if needed.

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { useTranslate } from 'react-admin';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import { useHistory } from 'react-router';
 
 import useAuthorized from '../../auth/useAuthorized';
 
 export default () => {
   useAuthorized();
   const t = useTranslate();
+
+  const history = useHistory();
+
+  const redirectPath = localStorage.getItem('appPath');
+
+  if (redirectPath) {
+    localStorage.removeItem('appPath');
+    history.push(`/${redirectPath}`);
+  }
 
   return (
     <Card>
