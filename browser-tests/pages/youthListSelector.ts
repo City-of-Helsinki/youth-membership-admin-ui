e0fba3d38a49bbdb870cc5ed29606807f31c4fac
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2020-10-08 09:33:51 +0300

    Improve search list e2e tests.

--- browser-tests/pages/youthListSelector.ts
+++ browser-tests/pages/youthListSelector.ts
@@ -2,7 +2,9 @@ import { Selector } from 'testcafe';
 
 export const youthListSelector = {
   firstName: Selector('input[id="firstName"]'),
+  lastName: Selector('input[id="lastName"]'),
   searchButton: Selector('button[class^="YouthList_search"]'),
   addNewButton: Selector('button[class^="YouthList_create"]'),
   dataGrid: Selector('div[class^="YouthList_dataGrid"]'),
+  selectTestProfile: Selector('td').withText('Existing'),
 };
--- browser-tests/youthListView.ts
+++ browser-tests/youthListView.ts
@@ -2,16 +2,28 @@ import { navigationSelector } from './pages/navigationSelector';
 import { login } from './util/login';
 import { testUrl } from './util/settings';
 import { youthListSelector } from './pages/youthListSelector';
+import { youthInformationSelector } from './pages/youthInformationSelector';
 
 fixture('Youth list view').page(testUrl());
 
-test('Test search funtionality', async (t) => {
+test('Test search functionality', async (t) => {
   await login(t);
 
   await t
     .click(navigationSelector.youthProfiles)
-    .typeText(youthListSelector.firstName, 'Testi')
+    .typeText(youthListSelector.firstName, 'Existing')
+    .typeText(youthListSelector.lastName, 'TestProfile')
     .click(youthListSelector.searchButton)
+    .expect(youthListSelector.dataGrid.exists)
+    .ok()
+    .click(youthListSelector.selectTestProfile)
+    .expect(youthInformationSelector.basicInformationTitle.exists)
+    .ok()
+    .click(youthInformationSelector.backToSearchResults)
+    .expect(youthListSelector.firstName.value)
+    .eql('Existing')
+    .expect(youthListSelector.lastName.value)
+    .eql('TestProfile')
     .expect(youthListSelector.dataGrid.exists)
     .ok();
 });
