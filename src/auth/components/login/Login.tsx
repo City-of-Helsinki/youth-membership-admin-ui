21bdbe813be042025494eaa881f791faf8d06ca2
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-11-03 12:22:33 +0200

    Refactor redirection

--- src/auth/components/login/Login.tsx
+++ src/auth/components/login/Login.tsx
@@ -3,6 +3,7 @@ import { Login, useTranslate, useAuthProvider } from 'react-admin';
 import { Button, Card, CardContent } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { makeStyles } from '@material-ui/core/styles';
+import { RouteComponentProps, StaticContext } from 'react-router';
 
 import theme from '../../../common/materialUI/themeConfig';
 
@@ -19,35 +20,41 @@ const useStyles = makeStyles({
   },
 });
 
-const LoginPageContent: React.FC = () => {
+type Props = RouteComponentProps<
+  {},
+  StaticContext,
+  {
+    nextPathname?: string;
+  }
+>;
+
+const LoginPage = ({ location }: Props) => {
   const classes = useStyles();
   const t = useTranslate();
   const authProvider = useAuthProvider();
+  const nextPathname = location.state?.nextPathname ?? undefined;
+
   return (
-    <ThemeProvider theme={theme}>
-      <Card>
-        <CardContent>
-          <div className={classes.container}>
-            <p>{t('dashboard.title')}</p>
-            <Button
-              className={classes.button}
-              variant="contained"
-              color="secondary"
-              onClick={() => authProvider.login()}
-            >
-              {t('ra.auth.sign_in')}
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-    </ThemeProvider>
+    <Login>
+      <ThemeProvider theme={theme}>
+        <Card>
+          <CardContent>
+            <div className={classes.container}>
+              <p>{t('dashboard.title')}</p>
+              <Button
+                className={classes.button}
+                variant="contained"
+                color="secondary"
+                onClick={() => authProvider.login(nextPathname)}
+              >
+                {t('ra.auth.sign_in')}
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </ThemeProvider>
+    </Login>
   );
 };
-
-const LoginPage: React.FC = () => (
-  <Login>
-    <LoginPageContent />
-  </Login>
-);
 
 export default LoginPage;
