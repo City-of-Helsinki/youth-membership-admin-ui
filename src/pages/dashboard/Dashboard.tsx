23202790230a2bf04d7f31bf6d70e3a85356e684
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-11-03 12:22:33 +0200

    Refactor redirection

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,25 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useTranslate } from 'react-admin';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import { useHistory } from 'react-router';
 
 import useAuthorized from '../../auth/useAuthorized';
 
 const JassariDashboard = () => {
   useAuthorized();
   const t = useTranslate();
-
-  const history = useHistory();
-
-  const redirectPath = localStorage.getItem('redirectPath');
-
-  useEffect(() => {
-    if (redirectPath) {
-      localStorage.removeItem('redirectPath');
-      history.push(`/${redirectPath}`);
-    }
-  }, [history, redirectPath]);
 
   return (
     <Card>
