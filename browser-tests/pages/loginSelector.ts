c5cdf0a62d766ad1fb8c9621227c9115656f56e0
Tero Virtanen <105207219+terovirtanen@users.noreply.github.com>
2023-03-20 09:43:52 +0200

    Browser tests on Platta review (#139)
    * github workflow setup DevOps review client id to tunnistamo
    * Browser test record video
    * Browser test login link class name

--- browser-tests/pages/loginSelector.ts
+++ browser-tests/pages/loginSelector.ts
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 export const loginSelector = {
   loginButton: Selector('button[type="button"]'),
-  helLoginLink: Selector('.login-method-helusername'),
+  helLoginLink: Selector('.btn-helusername'),
   helUsername: Selector('#username'),
   helPassword: Selector('#password'),
   helLogin: Selector('#kc-login'),
